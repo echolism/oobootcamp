34f56029d2b5f05adc90a8005513dfe716db062d
Oscar <21330054+echolism@users.noreply.github.com>
2018-01-24 22:05:34 +0800

    Refactor SmartParkingBoy

--- ParkingLot/src/main/oobootcamp/parkinglot/SmartParkingBoy.java
+++ ParkingLot/src/main/oobootcamp/parkinglot/SmartParkingBoy.java
@@ -1,5 +1,6 @@
 package oobootcamp.parkinglot;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,20 +8,8 @@ public class SmartParkingBoy extends ParkingBoy {
 
     @Override
     public Optional<ParkingLot> findTargetParkingLot() {
-        List<ParkingLot> parkingLots = getParkingLots();
-        if (parkingLots.size() == 1) {
-            return Optional.of(parkingLots.get(0));
-        }
-        if (parkingLots.size() == 2) {
-            return Optional.of(parkingLots.get(0).getNumberOfAvailableSlots() >= parkingLots.get(1).getNumberOfAvailableSlots() ? parkingLots.get(0) : parkingLots.get(1));
-        }
-        if (parkingLots.get(0).getNumberOfAvailableSlots() >= parkingLots.get(1).getNumberOfAvailableSlots() && parkingLots.get(0).getNumberOfAvailableSlots() >= parkingLots.get(2).getNumberOfAvailableSlots()) {
-            return Optional.of(parkingLots.get(0));
-        }
-        if (parkingLots.get(1).getNumberOfAvailableSlots() >= parkingLots.get(0).getNumberOfAvailableSlots() && parkingLots.get(1).getNumberOfAvailableSlots() >= parkingLots.get(2).getNumberOfAvailableSlots()) {
-            return Optional.of(parkingLots.get(1));
-        }
-        return Optional.of(parkingLots.get(2));
+        return getParkingLots().stream()
+                .max(Comparator.comparingInt(ParkingLot::getNumberOfAvailableSlots));
     }
 
 
